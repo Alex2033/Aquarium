d59a7e714b2ef855ec99bbb45cfe1a9e895ce241
Alexandr Khokhlov <XoxlofAlexandr@yandex.ru>
2018-09-01 17:17:57 +0300

    Сделаны адаптивно две секции: океанариум для детей и дайвинг. Добавлен scoll up

--- app/js/common.js
+++ app/js/common.js
@@ -10,3 +10,38 @@ $(function() {
 $('.toggle-menu').click(function() {
 	$('.menu--hidden').slideToggle();
 });
+
+$(function(){
+
+	$('.children__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		arrows: true,
+		prevArrow: ".children-arrow--left",
+		nextArrow: ".children-arrow--right",
+		dotsClass: "children-buttons",
+		customPaging : function(slider, i) {
+			var thumb = $(slider.$slides[i]).data('thumb');
+			return '<button class="children-button"></button>';
+},
+	});
+
+});
+
+$(function() {
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > $(".header").offset().top) {
+			$("#scrollUp").fadeIn();
+		}
+		else {
+			$("#scrollUp").fadeOut();
+		}
+	});
+	$("#scrollUp").click(function() {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 400);
+		return false;
+	});
+});
--- gulpfile.js
+++ gulpfile.js
@@ -53,6 +53,7 @@ gulp.task('js', function() {
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/font-awesome/fontawesome-all.min.js',
 		'app/libs/superfish/superfish.min.js',
+		'app/libs/slick/slick.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
